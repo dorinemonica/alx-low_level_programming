bcc912070b18c320b66249b76d8fe25c6c6d69ac
dorinemonica <njorogedorine@gmail.com>
2023-02-17 15:39:38 -0800

    Single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - main function entry
+ *
+ * printf displays the outcome
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
