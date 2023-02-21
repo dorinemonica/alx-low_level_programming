f6d2a041efdeca0dcc5829e9cd9544a71500cec2
dorinemonica <njorogedorine@gmail.com>
2023-02-21 13:59:46 -0800

    Putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - main function entry
+ *
+ * _putchar - writes the character c to stdout
+ *
+ * Return: (O) success
+ *
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
